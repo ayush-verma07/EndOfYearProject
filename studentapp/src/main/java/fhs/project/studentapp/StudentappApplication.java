45b112449a08555f313ce319218bde5139c44faa
Ayush Verma <107882615+ayush-verma07@users.noreply.github.com>
2024-04-16 22:48:43 -0500

    Update StudentappApplication.java

--- studentapp/src/main/java/fhs/project/studentapp/StudentappApplication.java
+++ studentapp/src/main/java/fhs/project/studentapp/StudentappApplication.java
@@ -1,1 +1,13 @@
+package fhs.project.studentapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentappApplication.class, args);
+	}
+
+}
